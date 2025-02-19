c9b79467c2c786c01684ac47dae33c751de0d86b
FrancoEspinozaV <142062208+FrancoEspinozaV@users.noreply.github.com>
2025-02-18 21:35:15 -0300

    chrore: add primer react, zustand, react form, react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import { PrimeReactProvider } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <PrimeReactProvider value={{
+      unstyled: true,
+      pt: Tailwind
+    }}>
+      <App />
+    </PrimeReactProvider>
+  </BrowserRouter>,
 )
